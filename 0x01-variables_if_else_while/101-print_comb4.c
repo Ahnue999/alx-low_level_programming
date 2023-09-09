b2d59fbce6c6ea814b6fdbaabe8f34c1fd19757d
Ahnue999 <ahnue200572@gmail.com>
2023-09-09 08:37:22 -0700

    commited

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = i + 2; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i == 7 && j == 8 && k == 9)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
