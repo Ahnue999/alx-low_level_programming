507985149b3666db1ec9ad67fb82b7a689f1b979
Ahnue999 <ahnue200572@gmail.com>
2023-09-12 11:06:56 -0700

    commited

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,33 @@
  */
 int main(void)
 {
-	int i = 0;
-	long int prev = 1;
-	long int n = 1;
-	long int tmp = 1;
+	int i, j;
+	unsigned long int x, y;
 
-	for (i = 0; i < 98; i++)
+	for (i = 1; i < 91; i++)
 	{
-		if (i < 97)
-			printf("%ld, ", n);
-		else
-			printf("%ld\n", n);
-		tmp = n;
-		n += prev;
-		prev = tmp;
+		printf("%lu, ", k);
+		k += j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
 
 	return (0);
 }
