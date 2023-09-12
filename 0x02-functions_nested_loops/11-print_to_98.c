dbeda5e62b1b0a602e05e852ebb221f5ef13eddf
Ahnue999 <ahnue200572@gmail.com>
2023-09-12 10:05:57 -0700

    commited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			if (n < 97)
 				printf("%d, ", n);
@@ -26,7 +26,7 @@ void print_to_98(int n)
 		}
 	}
 	else
-		while (n > 98)
+		while (n >= 98)
 		{
 			if (n > 99)
 				printf("%d, ", n);
