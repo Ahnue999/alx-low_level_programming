ef9c5be0178dd5c07bd8b4f292a0f4a16c1d88aa
Ahnue999 <ahnue200572@gmail.com>
2023-09-12 10:38:54 -0700

    commited

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,22 +9,42 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		int i, j;
+		int i, j, k;
 
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				_putchar((i * j) + '0');
-				if (j < n)
+				k = i * j;
+				if (j == 0)
+					_putchar(k + '0');
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 10 && k > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar((k % 100) + '0');
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
+}
 }
