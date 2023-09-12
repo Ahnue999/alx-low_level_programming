9d76249b01c8e4620654e2f1180fe2b245a64a12
Ahnue999 <ahnue200572@gmail.com>
2023-09-12 09:49:12 -0700

    commited

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,25 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
+			k = i * j;
+
+			if (k / 10 > 0)
+				_putchar((k / 10) + '0');
+			else
+				_putchar(' ');
+			
+			_putchar((k % 10) + '0');
+
+			
 			if (j < 9)
 			{
 				_putchar(',');
