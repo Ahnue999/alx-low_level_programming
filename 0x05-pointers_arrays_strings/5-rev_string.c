fa1818b5799980edb5166290e28be74aa27fd57b
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 12:42:10 -0700

    commited

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * rev_string - prints a string
- * @s: the string
- * Return: void
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
  */
 void rev_string(char *s)
 {
-	int count = 0, i,  j;
-	int temp;
+	int count = 0, i, j;
+	char *str, temp;
 
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
 			break;
-		i++;
+		count++;
 	}
+	str = s;
 
-	for (i = 0, j = count; i < count / 2; i++, j--)
+	for (i = 0; i < (count - 1); i++)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
-
 }
