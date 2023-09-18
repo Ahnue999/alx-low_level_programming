064bcc7377ce8a6d58bb55ff4d749fad349b4c18
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 12:03:14 -0700

    commited

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n + 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		_putchar(*(a + i));
+		if (i < n - 1)
+			printf("%d, ", *(a + i));
+		else
+			printf("%d", *(a + i));
 	}
 	_putchar('\n');
 }
