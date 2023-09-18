2f69c45fec6188ef20d7a7f66589ee9bbac1027a
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 10:57:28 -0700

    commited

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,15 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (i >= 0)
 	{
+		if (s[i] == '\0')
+			break;
 		i++;
 	}
 
-	for (; i > 0; i--)
-		_putchar(*(s + i));
+	for (i--; i > 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
