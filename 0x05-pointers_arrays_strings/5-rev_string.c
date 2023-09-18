d9fe9fb5bb3c5cf128be3e7665164aa15db74220
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 11:27:14 -0700

    commited

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int count, i,  j;
+	int temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		i++;
+	}
+
+	for (i = 0, j = count; i < count / 2; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+
+}
