eebaf455b0961f98725c4cc1f9bfe2bc7ea99d7e
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 09:56:28 -0700

    commited

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string
+  * @str: the string
+  * Return: void
+  */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		_putchar(*(s + i))
+	return (i);
+}
