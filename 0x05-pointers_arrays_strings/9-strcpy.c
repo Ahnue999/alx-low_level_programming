e7658298b6b5294c64e28e9630ffeb04013f0803
Ahnue999 <ahnue200572@gmail.com>
2023-09-18 12:23:52 -0700

    commited

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strcpy - prints a string
+  * @dest: string to copy to
+  * @src: string to copy from
+  * Return: pointer
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+
+	while (length >= 0)
+	{
+		if (*src == '\0')
+			length++;
+			break;
+		length++;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return *dest;
+}
