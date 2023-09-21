c58c5c9d4776f5c695aa415bd6e397d1f5f22c74
Ahnue999 <ahnue200572@gmail.com>
2023-09-21 08:28:51 -0700

    commited

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * _strcmp - compares to strings
-  * @s1: the first string
-  * @s1: the second string
-  * Return: 1 if the same and 0 otherwise
-  */
+ * _strcmp - compares to strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if the same and 0 otherwise
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i, j;
@@ -14,11 +14,13 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0')
 	{
-			if (s1[i] < s2[i])
-				return (-15);
-			else if (s1[i] > s2[i])
-				return (15);
+		if (s1[i] < s2[i])
+			return (-15);
+		else if (s1[i] > s2[i])
+			return (15);
 		i++;
 	}
+	if (s2[i] != '\0')
+		return (15);
 	return (0);
 }
