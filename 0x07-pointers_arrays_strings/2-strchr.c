802aa08707613edb789f4fa8c0081a9f2f1df78d
Ahnue999 <ahnue200572@gmail.com>
2023-09-25 11:03:18 -0700

    commited

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - finds a char in a string
+ * @s: the string
+ * @c: the character to find
+ * Return: the pointer to the first occurance
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p = 0;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+	}
+
+	return (p);
+}
