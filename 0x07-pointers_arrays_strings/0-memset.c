6b6cff8f36ffac99f18e24714fb0c032a390b8f5
Ahnue999 <ahnue200572@gmail.con>
2023-09-25 20:05:32 +0300

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,10 @@
 
 /*
  * _memset - sets a memory position to a byte
- *
+ * @s: the address to fill
+ * @b: the filler character
+ * @n: the number of bytes to fill
+ * Return: a pointer to the first byte
  */
 char *_memset(char *s, char b, unsigned int n)
 {
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,14 +5,14 @@
  * @dest: where copied memory is stored
  * @src: the adress to be copied
  * @n: the number of bytes to copy
- * Return : the resulting string
+ * Return : a pointer to the first byte
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-		*(s + i) = b;
+		*(dest + i) = *(src + i);
 
-	return (s)
+	return (dest);
 }
