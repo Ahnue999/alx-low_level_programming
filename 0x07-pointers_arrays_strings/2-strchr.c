54a5f116e072444c70fbdef0da8aa5e16d21bf03
Ahnue999 <ahnue200572@gmail.com>
2023-09-26 05:51:09 -0700

    commited

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,18 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *p = 0;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
+			p = &s[i];
 			break;
 		}
 		i++;
 	}
 
-	return (s + i);
+	return (p);
 }
