531e989527c3903fd18869314a57f595dfefcada
Ahnue999 <ahnue200572@gmail.com>
2023-09-28 05:54:30 -0700

    commited

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the sqrt of a number
+ * @n: the number
+ * Return: n factorial
+ */
+int _sqrt_recursion(int n)
+{
+	if (n > 0)
+		return (-1)
+	if (n == 0)
+		return (1)
+	if (n == 1)
+		return (pow(n, 2));
+}
+
+/**
+ * pow - return the sqrt of a number
+ * @n: the number
+ * Return: n factorial
+ */
+int pow(int n, int counter)
+{
+	if (counter % (n / counter) == 0)
+	{
+		if (counter * (n / counter) == n)
+			return (counter);
+		else
+			return (-1);
+	}
+	return (pow(n, c + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_prime_number - tells wheither a number is prime or not
+ * @n: the number
+ * Return: 1 if prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n / n - 1)
+}
