f93cb4beda541703fd88a89c6a0988ba08193dab
Ahnue999 <ahnue200572@gmail.com>
2023-09-27 23:59:01 -0700

    commited

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - prints a string using recurision
+  * _print_rev_recursion - prints a string in reverse using recurision
   * @s: the string to print
   * Return: void
   */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
 	_puts_recursion(s + 1);
+	_putchar(*s);
 }
