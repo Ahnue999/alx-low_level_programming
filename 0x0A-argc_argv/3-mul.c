14478d4fbc2197ee049361258809cd8bb7a67be6
Ahnue999 <ahnue200572@gmail.com>
2023-10-01 22:51:42 -0700

    commited

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main function
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0 when work
+  */
+int main(int argc , char* argv[]) 
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+
+	return (0);
+}
