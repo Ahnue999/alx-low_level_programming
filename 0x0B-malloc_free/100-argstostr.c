0d1e4a66fbc1368e7f23019eeb4b6d640d8c786b
Ahnue999 <ahnue200572@gmail.com>
2023-10-03 09:59:17 -0700

    commited

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates arguments
+ * @ac: argument count
+ * @av: argument vector
+ * Return: the concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, length, wlength;
+	int current;
+	char *conc;
+
+	length = wlength = current = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		length = length + strlen(av[i]) + 1;
+	}
+
+	conc = (char *)malloc(sizeof(char) * length);
+
+	if (conc == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		wlength = strlen(av[i]);
+		for (j = 0; j < wlength; j++, current++)
+		{
+			conc[current] = av[i][j];
+		}
+		if (i < ac - 1)
+		{
+			conc[current] = '\n';
+			current++;
+		}
+	}
+	conc[current] = '\0';
+
+	return (conc);
+}
