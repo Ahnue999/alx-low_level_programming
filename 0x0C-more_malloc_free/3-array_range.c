0b3e603ba0ad7b92a10f9ddf512e6e71e3262c58
Ahnue999 <ahnue200572@gmail.com>
2023-10-07 01:14:48 -0700

    commited

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -24,7 +24,7 @@ int *array_range(int min, int max)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; min <= size; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 		*(p + i) = min;
 
 	return (p);
