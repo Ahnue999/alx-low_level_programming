38f50ef1a9a54d7b4b6ff5094610daad42496029
Ahnue999 <ahnue200572@gmail.com>
2023-10-07 00:23:40 -0700

    commited

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+  * _calloc - allocate blocks of memory of a certain size
+  * @nmemb: the number of blocks
+  * @size: the size
+  * Return: a pointer to the allocated memory
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	int i, csize;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	csize = nmemb * size;
+
+	p = malloc(csize);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < csize; i++)
+		*(p + i) = 0;
+
+	return ((void *)p);
+}
