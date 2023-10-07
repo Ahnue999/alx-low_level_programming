f31c2d66317348dc78e0d53634d7c52f8c1f2d7b
Ahnue999 <ahnue200572@gmail.com>
2023-10-07 02:35:54 -0700

    commited

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
-		malloc(new_size);
+		ptr = malloc(new_size);
 
 	if (new_size == old_size)
 		return (ptr);
