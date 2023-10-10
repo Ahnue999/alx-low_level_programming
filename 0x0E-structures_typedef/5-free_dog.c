924569544ecb5cc2ab590b4652c0ecc66455b607
Ahnue999 <ahnue200572@gmail.com>
2023-10-10 11:33:42 -0700

    commited

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,10 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * free_dog - frees dogs.
- * @d: struct dog.
- *
- * Return: no return.
+ * free_dog - frees dogs
+ * @d: the structure
+ * Return: no return
  */
 void free_dog(dog_t *d)
 {
