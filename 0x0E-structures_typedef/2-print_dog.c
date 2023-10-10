6562fce8c2f6eae42c4c1577e3bda86b697c0c04
Ahnue999 <ahnue200572@gmail.com>
2023-10-10 11:00:40 -0700

    commited

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,13 @@ void print_dog(struct dog *d)
 		if (!d->name)
 			printf("Name: (nil)\n");
 		else
-			printf("Name: %s\n",d->name);
+			printf("Name: %s\n", d->name);
 
-		printf("Age: %f\n",d->age);
+		printf("Age: %f\n", d->age);
 
 		if (!d->owner)
 			printf("Name: (nil)\n");
 		else
-			printf("Onwer: %s\n",d->owner);
+			printf("Onwer: %s\n", d->owner);
 	}
 }
