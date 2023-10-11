cdbee0d644ccb91a2dde70fd62768c2910f5ae40
Ahnue999 <ahnue200572@gmail.com>
2023-10-11 09:41:48 -0700

    commited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
   * print_name - a function that prints a name
   * @name: the name to print
   * @f: the function to use
+  *
   * Return: no return
   */
 void print_name(char *name, void (*f)(char *))
