98099e9e98595e5375946de83b6d543428d053b5
Ahnue999 <ahnue200572@gmail.com>
2023-10-10 22:40:56 -0700

    commited

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - commits an action to all array members
+  * @array: the array
+  * @size: the size of the array
+  * @action: the action to commit
+  * Return: no return
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
