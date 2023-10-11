f16663c0adc54e8bda2e8eb200a46218eb5d7d62
Ahnue999 <ahnue200572@gmail.com>
2023-10-11 09:29:30 -0700

    commited

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,23 +2,26 @@
 #include "function_pointers.h"
 
 /**
-  * int_index - search an int in an array
-  * @array: the array
-  * @size: the size of the array
-  * @cmp: the comparesion function
-  * Return: the first index where the int is found
-  */
+ * int_index - search an int in an array
+ * @array: the array
+ * @size: the size of the array
+ * @cmp: the comparesion function
+ * Return: the first index where the int is found
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 	return (-1);
 }
