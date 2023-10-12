e4169408c274d3a38ae495f30679e43bb1268a12
Ahnue999 <ahnue200572@gmail.com>
2023-10-12 00:35:25 -0700

    commited

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+  * sum_them_all - sum all its arguments.
+  * @n: the first argument.
+  *
+  * Return : the sum.
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+
+	return (sum);
+}
