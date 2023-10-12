b4524a88dbd9459e02820a8cd8e049fa06b542bf
Ahnue999 <ahnue200572@gmail.com>
2023-10-12 00:47:48 -0700

    commited

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,27 @@
 #include "variadic_functions.h"
 
 /**
-  * print_numbers - sum all its arguments.
-  * @n: the first argument.
-  *
-  * Return : the sum.
-  */
+ * print_numbers - sum all its arguments.
+ * @n: the first argument.
+ * @separator: the separtaor.
+ *
+ * Return : No return.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
 	va_list ap;
 
-	if (n == 0)
-		return (0);
+	if (separator == NULL)
+		separator = "";
 
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-
-	return (sum);
+	{
+		if (i < n - 1)
+			printf("%d%s", va_arg(ap, int), separator);
+		else
+			printf("%d\n", va_arg(ap, int));
+	}
 }
+
