3200f02e96db50df7c30e9b2528a02943add73d8
Ahnue999 <ahnue200572@gmail.com>
2023-10-19 11:24:28 -0700

    commited

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - frees a list.
+ * @head: the list pointer (head).
+ * Return: Nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
