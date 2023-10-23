3703b3de617b726e0f86ff50a48934aa0dcea357
Ahnue999 <ahnue200572@gmail.con>
2023-10-23 07:23:44 +0300

    Task 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list.
+ *
+ * @head: the head pointer.
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head);
+{
+	listint_t *ptr;
+
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+	ptr = NULL;
+}
