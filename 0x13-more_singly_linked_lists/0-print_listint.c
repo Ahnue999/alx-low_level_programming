b0594f71b106fe7dbe42a3284d8b20ca55c163f0
Ahnue999 <ahnue200572@gmail.com>
2023-10-24 19:09:04 -0700

    commited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
-	const listint_t *ptr;
 
-	if (h == NULL)
-		return (-1);
-
-	ptr = h;
-
-	while (ptr)
+	while (h)
 	{
 		printf("%d\n", ptr->n);
-		ptr = ptr->next;
+		h = h->next;
 		i++;
 	}
 
