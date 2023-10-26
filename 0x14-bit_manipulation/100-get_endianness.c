acced5d9362497a6c72645a77447c8af0466c988
Ahnue999 <ahnue200572@gmail.com>
2023-10-26 05:50:37 -0700

    commited

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_endianness - tells the endianness of an architecture.
+  *
+  * Return: nothing.
+  */
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *)&x;
+
+	if (*c = 10)
+		return (1);
+	else
+		return (0);
+}
