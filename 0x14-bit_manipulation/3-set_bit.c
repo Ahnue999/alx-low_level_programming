9e7378271ca7e6ecc32c95e5c0ae9359aa15d858
Ahnue999 <ahnue200572@gmail.con>
2023-10-26 13:05:17 +0300

    set and clear

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - set a bit at a certain index to [1].
+ * @n: the integer.
+ * @index: the index to set its bit.
+ *
+ * Return: the unsigned integer.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int count = 1;
+
+	while (index)
+	{
+		count *= 2;
+		index--;
+	}
+
+	*n = *n | count;
+	return (1);
+}
