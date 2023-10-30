c2f647b6871ceca3efa9dcf7fc2f9fb04601d9c0
Ahnue999 <ahnue200572@gmail.con>
2023-10-30 21:25:53 +0300

    edit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
