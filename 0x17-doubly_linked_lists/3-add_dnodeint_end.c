988d5e2072fd82f507b99fa8b4c4097c16fd4d46
Ahnue999 <ahnue200572@gmail.com>
2023-12-07 01:59:50 -0800

    committed

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		curr = *head;
-		while(curr->next)
+		while (curr->next)
 			curr = curr->next;
 		curr->next = new;
 		new->prev = curr;
