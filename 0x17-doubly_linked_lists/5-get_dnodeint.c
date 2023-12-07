44749ccddd3af4e093d732e73cee979f09093bd3
Ahnue999 <ahnue200572@gmail.com>
2023-12-07 02:12:11 -0800

    committed

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	for (curr = head; index > 0 && curr->next; curr = curr->next, index--)
 		;
-	if (index)
+	if (index != 0)
 		return (NULL);
 
 	return (curr);
