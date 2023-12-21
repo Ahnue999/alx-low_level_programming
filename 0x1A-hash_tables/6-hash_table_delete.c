d64b559ea7761d5b0a379f57e5f9e3cc35869301
Ahnue999 <ahnue200572@gmail.con>
2023-12-21 17:01:56 +0300

    commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * free_list - frees a list.
+ * @head: the head pointer.
+ *
+ * Return: void.
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+	}
+}
+
+/**
  * hash_table_delete - deletes the hash table.
  * @ht: the hash table to delete.
  *
@@ -9,20 +29,9 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t **list, *tmp;
 
 	for (i = 0; i < ht->size; i++)
-	{
-		list = &(ht->array[i]);
-		tmp = *list;
-		while (*list)
-		{
-			*list = ((*list)->next);
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
-		}
-	}
+		free_list(ht->array[i]);
 	free(ht->array);
 	free(ht);
 }
