62760269ddb4b2d905a8f86db62a6b3ec2363835
Ahnue999 <ahnue200572@gmail.con>
2023-12-23 04:46:12 +0300

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
+	hash_node_t **arr;
 	unsigned long int i;
 
 	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
 
-	new->array = malloc(sizeof(hash_node_t *) * size);
-	if (!(new->array))
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		new->array[i] = NULL;
+		arr[i] = NULL;
 
+	new->array = arr;
 	new->size = size;
 
 	return (new);
