6aa2e9fcbeb0270da4c4b14890108bf548c3d2dd
Ahnue999 <ahnue200572@gmail.com>
2023-12-22 18:00:04 -0800

    committed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,24 +9,21 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *curr;
-	int first = 1;
 	unsigned long int i;
+	char *delim;
 
 	if (!ht)
 		return;
 
+	delim = ""
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		curr = ht->array[i];
 		while (curr)
 		{
-			if (first)
-			{
-				printf("'%s': '%s'", curr->key, curr->value);
-				first = 0;
-			}
-			printf(", '%s': '%s'", curr->key, curr->value);
+			printf("%s'%s': '%s'", delim, curr->key, curr->value);
+			delim = ", ";
 			curr = curr->next;
 		}
 	}
